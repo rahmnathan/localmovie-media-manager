9b9bb17c238692f59155bc1ef3955bd2a7649e23
nathan <rahm.nathan@gmail.com>
2018-08-08 18:40:12 -0500

    Config update

--- src/main/java/com/github/rahmnathan/Application.java
+++ src/main/java/com/github/rahmnathan/Application.java
@@ -2,8 +2,9 @@ package com.github.rahmnathan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jsonb.JsonbAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JsonbAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
