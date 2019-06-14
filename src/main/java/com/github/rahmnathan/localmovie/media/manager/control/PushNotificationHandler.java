da803ca1fcaef14068bfc72330ca9360320898a0
nathan <rahm.nathan@gmail.com>
2019-06-13 19:00:34 -0500

    Added ability to disable notifications.

--- src/main/java/com/github/rahmnathan/localmovie/media/manager/control/PushNotificationHandler.java
+++ src/main/java/com/github/rahmnathan/localmovie/media/manager/control/PushNotificationHandler.java
@@ -6,6 +6,7 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,20 +14,24 @@ public class PushNotificationHandler {
     private final Logger logger = LoggerFactory.getLogger(PushNotificationHandler.class);
     private static final String MOVIE_TOPIC = "movies";
     private final FirebaseNotificationService notificationService;
+    private final boolean enabled;
 
-    public PushNotificationHandler(ProducerTemplate template, CamelContext context) {
+    public PushNotificationHandler(ProducerTemplate template, CamelContext context, @Value("${push.notification.enabled:false}") boolean enabled) {
         this.notificationService = new FirebaseNotificationService(template, context);
+        this.enabled = enabled;
     }
 
     void sendPushNotifications(String fileName, String path) {
-        logger.info("Sending notification of new movie: {} to {} clients", path, MOVIE_TOPIC);
-        PushNotification pushNotification = PushNotification.Builder.newInstance()
-                .setTopic(MOVIE_TOPIC)
-                .setTitle("New Movie!")
-                .setBody(fileName)
-                .addData("path", path)
-                .build();
+        if (enabled) {
+            logger.info("Sending notification of new movie: {} to {} clients", path, MOVIE_TOPIC);
+            PushNotification pushNotification = PushNotification.Builder.newInstance()
+                    .setTopic(MOVIE_TOPIC)
+                    .setTitle("New Movie!")
+                    .setBody(fileName)
+                    .addData("path", path)
+                    .build();
 
-        notificationService.sendPushNotification(pushNotification);
+            notificationService.sendPushNotification(pushNotification);
+        }
     }
 }
