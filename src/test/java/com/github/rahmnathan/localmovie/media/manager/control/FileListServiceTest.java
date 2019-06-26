0e2c3a5e09baba8f0a3c9ecfd283b3bae690b46e
nathan <rahm.nathan@gmail.com>
2019-06-25 20:50:07 -0500

    Updated unit tests.

--- src/test/java/com/github/rahmnathan/localmovie/media/manager/control/FileListServiceTest.java
+++ src/test/java/com/github/rahmnathan/localmovie/media/manager/control/FileListServiceTest.java
@@ -1,20 +1,23 @@
 package com.github.rahmnathan.localmovie.media.manager.control;
 
+import com.github.rahmnathan.localmovie.media.manager.config.MediaManagerConfig;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileListServiceTest {
 
-//    @Test
-//    public void listFilesTest(){
-//        FileListService fileListService = new FileListService(new String[]{File.separator});
-//
-//        Set<String> fileList = fileListService.listFiles("/test/LocalMedia/something");
-//        assertNotNull(fileList);
-//    }
+    @Test
+    public void listFilesTest(){
+        MediaManagerConfig config = new MediaManagerConfig();
+        config.setMediaPaths(new String[]{File.separator});
+
+        FileListService fileListService = new FileListService(config);
+
+        Set<String> fileList = fileListService.listFiles("/test/LocalMedia/something");
+        assertNotNull(fileList);
+    }
 }
--- src/test/java/com/github/rahmnathan/localmovie/media/manager/control/MediaDirectoryMonitorTest.java
+++ src/test/java/com/github/rahmnathan/localmovie/media/manager/control/MediaDirectoryMonitorTest.java
@@ -1,5 +1,6 @@
 package com.github.rahmnathan.localmovie.media.manager.control;
 
+import com.github.rahmnathan.localmovie.media.manager.config.MediaManagerConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,16 +13,20 @@ import static org.mockito.Mockito.when;
 public class MediaDirectoryMonitorTest {
     private MediaDirectoryMonitor mediaDirectoryMonitor;
 
-//    @BeforeEach
-//    public void initialize(){
-//        FileListService fileListService = mock(FileListService.class);
-//        when(fileListService.listFiles(any())).thenReturn(Collections.emptySet());
-//        this.mediaDirectoryMonitor = new MediaDirectoryMonitor(Collections.emptySet(), new String[]{"/my/test/LocalMedia/"}, fileListService,
-//                mock(MediaCacheService.class), mock(MediaDataService.class));
-//    }
-//
-//    @Test
-//    public void initializeFileListTest(){
-//        mediaDirectoryMonitor.initializeFileList();
-//    }
+    @BeforeEach
+    public void initialize(){
+        FileListService fileListService = mock(FileListService.class);
+        when(fileListService.listFiles(any())).thenReturn(Collections.emptySet());
+
+        MediaManagerConfig config = new MediaManagerConfig();
+        config.setMediaPaths(new String[]{"/my/test/LocalMedia/"});
+
+        this.mediaDirectoryMonitor = new MediaDirectoryMonitor(Collections.emptySet(), config, fileListService,
+                mock(MediaCacheService.class), mock(MediaDataService.class));
+    }
+
+    @Test
+    public void initializeFileListTest(){
+        mediaDirectoryMonitor.initializeFileList();
+    }
 }
