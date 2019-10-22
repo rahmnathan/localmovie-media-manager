6e98fc931796a380e0d8474ab006b865798ea8b5
nathan <rahm.nathan@gmail.com>
2019-10-22 17:47:33 -0500

    Added MediaUser query for adding views.

--- src/main/java/com/github/rahmnathan/localmovie/persistence/control/MediaPersistenceService.java
+++ src/main/java/com/github/rahmnathan/localmovie/persistence/control/MediaPersistenceService.java
@@ -4,6 +4,8 @@ import com.github.rahmnathan.localmovie.persistence.entity.*;
 import com.github.rahmnathan.localmovie.persistence.repository.MediaFileEventRepository;
 import com.github.rahmnathan.localmovie.persistence.repository.MediaFileRepository;
 import com.github.rahmnathan.localmovie.data.MediaRequest;
+import com.github.rahmnathan.localmovie.persistence.repository.MediaUserRepository;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
@@ -17,21 +19,19 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.File;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static com.github.rahmnathan.localmovie.control.MediaDirectoryMonitor.ROOT_MEDIA_FOLDER;
 import static com.github.rahmnathan.localmovie.data.MediaOrder.SEASONS_EPISODES;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class MediaPersistenceService {
     private final Logger logger = LoggerFactory.getLogger(MediaPersistenceService.class);
     private final MediaFileEventRepository mediaFileEventRepository;
     private final MediaFileRepository mediaFileRepository;
-
-    public MediaPersistenceService(MediaFileEventRepository mediaFileEventRepository, MediaFileRepository mediaFileRepository) {
-        this.mediaFileEventRepository = mediaFileEventRepository;
-        this.mediaFileRepository = mediaFileRepository;
-    }
+    private final MediaUserRepository mediaUserRepository;
 
     public byte[] getMediaImage(String path){
         return mediaFileRepository.findMediaPosterByPath(path);
@@ -71,7 +71,7 @@ public class MediaPersistenceService {
         String relativePath = path.split(ROOT_MEDIA_FOLDER)[1];
         MediaFile mediaFile = mediaFileRepository.findByPath(relativePath, userName);
         if(mediaFile.getMediaViews().isEmpty()){
-            MediaUser mediaUser = new MediaUser(userName);
+            MediaUser mediaUser = mediaUserRepository.findByUserId(userName).orElse(new MediaUser(userName));
             MediaView mediaView = new MediaView(mediaFile, mediaUser, position);
             mediaFile.addMediaView(mediaView);
             mediaUser.addMediaView(mediaView);
--- src/main/java/com/github/rahmnathan/localmovie/persistence/repository/MediaUserRepository.java
+++ src/main/java/com/github/rahmnathan/localmovie/persistence/repository/MediaUserRepository.java
@@ -4,6 +4,9 @@ import com.github.rahmnathan.localmovie.persistence.entity.MediaUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MediaUserRepository extends JpaRepository<MediaUser, Long> {
+    Optional<MediaUser> findByUserId(String userId);
 }
