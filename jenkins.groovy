c7eacd16df3fca3dd9e165039ec84ed2dedb104c
nathan <rahm.nathan@gmail.com>
2019-11-17 21:09:22 -0600

    Updated Jenkins pipeline script.

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,23 +1,43 @@
 node {
-   def mvnHome
-   stage('Checkout') {
-      git 'https://github.com/rahmnathan/localmovie-media-manager.git'  
-      mvnHome = tool 'Maven'
-   }
-   stage('Compile') {
-      jdk = tool name: 'Java 11'
-      env.JAVA_HOME = "${jdk}"
-      sh "'${mvnHome}/bin/mvn' clean install"
-   }
-   stage('Docker Build') {
-      jdk = tool name: 'Java 11'
-      env.JAVA_HOME = "${jdk}"
-      sh "'${mvnHome}/bin/mvn' dockerfile:build"
-   }
+    def mvnHome
+    def jdk
+    stage('Setup') {
+        mvnHome = tool 'Maven'
+        jdk = tool name: 'Java 11'
+        env.JAVA_HOME = "${jdk}"
+    }
+    stage('Checkout') {
+        git 'https://github.com/rahmnathan/localmovie-media-manager.git'
+    }
+    stage('Set Version') {
+        PROJECT_VERSION = sh(
+                script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
+                returnStdout: true
+        ).trim()
+        env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
+        sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
+    }
+    stage('Tag') {
+        sh 'git config --global user.email "rahm.nathan@gmail.com"'
+        sh 'git config --global user.name "rahmnathan"'
+        sshagent(credentials: ['Github-Git']) {
+            sh 'mkdir -p /home/jenkins/.ssh'
+            sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
+            sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+        }
+    }
+    stage('Package') {
+        sh "'${mvnHome}/bin/mvn' clean package -DskipTests"
+    }
+    stage('Test') {
+        sh "'${mvnHome}/bin/mvn' test"
+    }
+    stage('Docker Build') {
+        sh "'${mvnHome}/bin/mvn' dockerfile:build"
+    }
     withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: 'Dockerhub',
                       usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
         stage('Docker Push') {
-
             sh "'${mvnHome}/bin/mvn' dockerfile:push -Ddockerfile.username=$USERNAME -Ddockerfile.password='$PASSWORD'"
         }
     }
