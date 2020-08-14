67bff923e4c5b5e18b50aff249fd7168585b1ab5
nathan <rahm.nathan@gmail.com>
2020-08-14 16:19:50 -0500

    Base cookie implementation.

--- src/main/js/VideoPlayer.jsx
+++ src/main/js/VideoPlayer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { buildPosterUri } from "./Media.jsx";
+import ReactPlayer from 'react-player'
+import Cookies from 'universal-cookie';
 
 const videoBaseUri = '/localmovie/v3/media/';
+
+const cookies = new Cookies();
 
 const backgroundTintStyle = {
     zIndex: -1,
@@ -12,14 +16,6 @@ const backgroundTintStyle = {
     top: 0,
     left: 0,
     background: 'rgba(0, 0, 0, 0.7)'
-};
-
-const buildVideoPath = function (mediaFileId) {
-    if(mediaFileId !== null) {
-        return videoBaseUri + encodeURIComponent(mediaFileId) + "/stream.mp4";
-    } else {
-        return videoBaseUri;
-    }
 };
 
 export const viewingVideos = function (path) {
@@ -33,6 +29,7 @@ export const viewingVideos = function (path) {
 const videoPlayerStyle = {
     position: 'absolute',
     width: '80%',
+    height: '80%',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
@@ -50,6 +47,8 @@ export class VideoPlayer extends React.Component {
     constructor(props) {
         super(props);
         this.handleVideoMounted = this.handleVideoMounted.bind(this);
+        this.saveProgress = this.saveProgress.bind(this);
+        this.buildVideoPath = this.buildVideoPath.bind(this);
     }
 
     handleVideoMounted(element) {
@@ -59,12 +58,28 @@ export class VideoPlayer extends React.Component {
         }
     };
 
+    saveProgress(content) {
+        cookies.set('progress-' + this.props.mediaFileId, content.playedSeconds, {sameSite: 'strict'});
+    };
+
+    buildVideoPath(mediaFileId) {
+        if(mediaFileId !== null) {
+            return videoBaseUri + encodeURIComponent(mediaFileId) + "/stream.mp4#t=" + (cookies.get('progress-' + this.props.mediaFileId) || 0);
+        } else {
+            return videoBaseUri;
+        }
+    };
+
     render() {
         return (
             <div style={videoPlayerStyle}>
-                <video width="100%" controls poster={buildPosterUri(this.props.mediaFileId)} onLoadedMetadata={this.handleVideoMounted}>
-                    <source src={buildVideoPath(this.props.mediaFileId)} type="video/mp4"/>
-                </video>
+                <ReactPlayer
+                    url={this.buildVideoPath(this.props.mediaFileId)}
+                    light={buildPosterUri(this.props.mediaFileId)}
+                    controls={true}
+                    width={'100%'}
+                    height={'100%'}
+                    onProgress={this.saveProgress}/>
                 <button style={buttonStyle} onClick={() => {
                     window.history.back()
                 }}>Exit Video
