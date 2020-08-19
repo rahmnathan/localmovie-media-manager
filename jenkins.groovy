599a54fa8d085d1ac7c8076b11f7765fc6fe6f4b
nathan <nathan.rahm@us.idemia.com>
2020-08-18 19:05:34 -0500

    Fixing Jenkins pipeline.

--- jenkins.groovy
+++ jenkins.groovy
@@ -13,35 +13,47 @@ pipeline {
             }
         }
         stage('Set Version') {
-            PROJECT_VERSION = sh(
-                    script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
-                    returnStdout: true
-            ).trim()
-            env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
-            sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
+            steps {
+                PROJECT_VERSION = sh(
+                        script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
+                        returnStdout: true
+                ).trim()
+                env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
+                sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
+            }
         }
         stage('Tag') {
-            sh 'git config --global user.email "rahm.nathan@gmail.com"'
-            sh 'git config --global user.name "rahmnathan"'
-            sshagent(credentials: ['Github-Git']) {
-                sh 'mkdir -p /home/jenkins/.ssh'
-                sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
-                sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+            steps {
+                sh 'git config --global user.email "rahm.nathan@gmail.com"'
+                sh 'git config --global user.name "rahmnathan"'
+                sshagent(credentials: ['Github-Git']) {
+                    sh 'mkdir -p /home/jenkins/.ssh'
+                    sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
+                    sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+                }
             }
         }
         stage('Package') {
-            sh "'${mvnHome}/bin/mvn' clean install -DskipTests"
+            steps {
+                sh "'${mvnHome}/bin/mvn' clean install -DskipTests"
+            }
         }
         stage('Test') {
-            sh "'${mvnHome}/bin/mvn' test"
+            steps {
+                sh "'${mvnHome}/bin/mvn' test"
+            }
         }
         stage('Docker Build') {
-            sh "'${mvnHome}/bin/mvn' dockerfile:build"
+            steps {
+                sh "'${mvnHome}/bin/mvn' dockerfile:build"
+            }
         }
         withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: 'Dockerhub',
                           usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
             stage('Docker Push') {
-                sh "'${mvnHome}/bin/mvn' dockerfile:push -Ddockerfile.username=$USERNAME -Ddockerfile.password='$PASSWORD'"
+                steps {
+                    sh "'${mvnHome}/bin/mvn' dockerfile:push -Ddockerfile.username=$USERNAME -Ddockerfile.password='$PASSWORD'"
+                }
             }
         }
         stage('Deploy to Kubernetes') {
@@ -49,17 +61,23 @@ pipeline {
                     file(credentialsId: 'Kubeconfig', variable: 'KUBE_CONFIG'),
                     string(credentialsId: 'VaultToken', variable: 'VAULT_TOKEN')
             ]) {
-                sh 'helm upgrade --install -n localmovies localmovies ./target/classes/localmovies/ --set localmovies.vaultToken=$VAULT_TOKEN --kubeconfig $KUBE_CONFIG'
+                steps {
+                    sh 'helm upgrade --install -n localmovies localmovies ./target/classes/localmovies/ --set localmovies.vaultToken=$VAULT_TOKEN --kubeconfig $KUBE_CONFIG'
+                }
             }
         }
         stage('Wait for Deployment') {
-            sh 'sleep 60s'
+            steps {
+                sh 'sleep 60s'
+            }
         }
         stage('Functional Test') {
             withCredentials([
                     usernamePassword(credentialsId: 'LocalMoviesCredentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
             ]) {
-                sh 'jmeter -n -t src/test/jmeter/localmovie-web-test.jmx -Jusername=$USERNAME -Jpassword=$PASSWORD'
+                steps {
+                    sh 'jmeter -n -t src/test/jmeter/localmovie-web-test.jmx -Jusername=$USERNAME -Jpassword=$PASSWORD'
+                }
             }
         }
     }
