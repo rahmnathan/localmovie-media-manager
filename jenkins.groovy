09abafad0e336072f97ae354bb070262a819d0ce
nathan <nathan.rahm@us.idemia.com>
2020-08-18 21:08:19 -0500

    Updating email plugin.

--- jenkins.groovy
+++ jenkins.groovy
@@ -66,7 +66,7 @@ node {
                 subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                 body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
         <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
-                recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+                to: 'rahm.nathan@protonmail.com'
         )
         throw e
     }
