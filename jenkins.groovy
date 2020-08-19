1280d2af97b2c963ef40b12b28c01830baf38741
nathan <nathan.rahm@us.idemia.com>
2020-08-18 19:32:30 -0500

    Attempting email notification on failed build.

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,62 +1,73 @@
 node {
     def mvnHome
     def jdk
-    stage('Setup') {
-        mvnHome = tool 'Maven'
-        jdk = tool name: 'Java 14'
-        env.JAVA_HOME = "${jdk}"
-    }
-    stage('Checkout') {
-        git 'https://github.com/rahmnathan/localmovie-media-manager.git'
-    }
-    stage('Set Version') {
-        PROJECT_VERSION = sh(
-                script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
-                returnStdout: true
-        ).trim()
-        env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
-        sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
-    }
-    stage('Tag') {
-        sh 'git config --global user.email "rahm.nathan@gmail.com"'
-        sh 'git config --global user.name "rahmnathan"'
-        sshagent(credentials: ['Github-Git']) {
-            sh 'mkdir -p /home/jenkins/.ssh'
-            sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
-            sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+    try {
+        stage('Setup') {
+            mvnHome = tool 'Maven'
+            jdk = tool name: 'Java 14'
+            env.JAVA_HOME = "${jdk}"
         }
-    }
-    stage('Package') {
-        sh "'${mvnHome}/bin/mvn' clean install -DskipTests"
-    }
-    stage('Test') {
-        sh "'${mvnHome}/bin/mvn' test"
-    }
-    stage('Docker Build') {
-        sh "'${mvnHome}/bin/mvn' dockerfile:build"
-    }
-    withCredentials([[$class : 'UsernamePasswordMultiBinding', credentialsId: 'Dockerhub',
-                      usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-        stage('Docker Push') {
-            sh "'${mvnHome}/bin/mvn' dockerfile:push -Ddockerfile.username=$USERNAME -Ddockerfile.password='$PASSWORD'"
+        stage('Checkout') {
+            git 'https://github.com/rahmnathan/localmovie-media-manager.git'
         }
-    }
-    stage('Deploy to Kubernetes') {
-        withCredentials([
-                file(credentialsId: 'Kubeconfig', variable: 'KUBE_CONFIG'),
-                string(credentialsId: 'VaultToken', variable: 'VAULT_TOKEN')
-        ]) {
-            sh 'helm upgrade --install -n localmovies localmovies ./target/classes/localmovies/ --set localmovies.vaultToken=$VAULT_TOKEN --kubeconfig $KUBE_CONFIG'
+        stage('Set Version') {
+            PROJECT_VERSION = sh(
+                    script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
+                    returnStdout: true
+            ).trim()
+            env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
+            sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
         }
-    }
-    stage('Wait for Deployment') {
-        sh 'sleep 60s'
-    }
-    stage('Functional Test') {
-        withCredentials([
-                usernamePassword(credentialsId: 'LocalMoviesCredentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
-        ]) {
-            sh 'jmeter -n -t src/test/jmeter/localmovie-web-test.jmx -Jusername=$USERNAME -Jpassword=$PASSWORD'
+        stage('Tag') {
+            sh 'git config --global user.email "rahm.nathan@gmail.com"'
+            sh 'git config --global user.name "rahmnathan"'
+            sshagent(credentials: ['Github-Git']) {
+                sh 'mkdir -p /home/jenkins/.ssh'
+                sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
+                sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+            }
         }
+        stage('Package') {
+            sh "'${mvnHome}/bin/mvn' clean install -DskipTests"
+        }
+        stage('Test') {
+            sh "'${mvnHome}/bin/mvn' test"
+        }
+        stage('Docker Build') {
+            sh "'${mvnHome}/bin/mvn' dockerfile:build"
+        }
+        withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: 'Dockerhub',
+                          usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            stage('Docker Push') {
+                sh "'${mvnHome}/bin/mvn' dockerfile:push -Ddockerfile.username=$USERNAME -Ddockerfile.password='$PASSWORD'"
+            }
+        }
+        stage('Deploy to Kubernetes') {
+            withCredentials([
+                    file(credentialsId: 'Kubeconfig', variable: 'KUBE_CONFIG'),
+                    string(credentialsId: 'VaultToken', variable: 'VAULT_TOKEN')
+            ]) {
+                sh 'helm upgrade --install -n localmovies localmovies ./target/classes/localmovies/ --set localmovies.vaultToken=$VAULT_TOKEN --kubeconfig $KUBE_CONFIG'
+            }
+        }
+        stage('Wait for Deployment') {
+            sh 'sleep 60s'
+        }
+        stage('Functional Test') {
+            withCredentials([
+                    usernamePassword(credentialsId: 'LocalMoviesCredentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
+            ]) {
+                sh 'jmeter -n -t src/test/jmeter/localmovie-web-test.jmx -Jusername=$USERNAME -Jpassword=$PASSWORD'
+            }
+        }
+    } catch(e) {
+        currentBuild.result = "FAILED"
+        emailext (
+                subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
+                recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+        )
+        throw e
     }
 }
