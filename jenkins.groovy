f80e6ad7635a05a22548f7988104d862335fc1f9
nathan <rahm.nathan@gmail.com>
2021-02-22 10:52:15 -0600

    Back to Java 11.

--- jenkins.groovy
+++ jenkins.groovy
@@ -8,7 +8,7 @@ node {
     try {
         stage('Setup') {
             mvnHome = tool 'Maven'
-            jdk = tool name: 'Java 15'
+            jdk = tool name: 'Java 11'
             env.JAVA_HOME = "${jdk}"
 
             server = Artifactory.server 'Artifactory'
