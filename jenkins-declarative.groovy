289e69aab16da783fb7b91d801e4430d0f54d9fa
nathan <rahm.nathan@gmail.com>
2021-02-22 20:43:19 -0600

    Removed duplicate stage.

--- jenkins-declarative.groovy
+++ jenkins-declarative.groovy
@@ -63,20 +63,6 @@ spec:
                 }
             }
         }
-        stage('Tag') {
-            steps {
-                script {
-                    def mvnHome = tool 'Maven'
-                    sh 'git config --global user.email "rahm.nathan@gmail.com"'
-                    sh 'git config --global user.name "rahmnathan"'
-                    sshagent(credentials: ['Github-Git']) {
-                        sh 'mkdir -p /home/jenkins/.ssh'
-                        sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
-                        sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
-                    }
-                }
-            }
-        }
         stage('Unit Test') {
             steps {
                 sh 'mvn test'
