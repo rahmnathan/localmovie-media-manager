03df25f2c83197621743754620a9a5c9eb9fe5a3
nathan <rahm.nathan@gmail.com>
2021-02-22 21:08:52 -0600

    Maven updates.

--- jenkins-declarative.groovy
+++ jenkins-declarative.groovy
@@ -38,26 +38,24 @@ spec:
         stage('Set Version') {
             steps {
                 script {
-                    def mvnHome = tool 'Maven'
                     PROJECT_VERSION = sh(
-                            script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
+                            script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout",
                             returnStdout: true
                     ).trim()
                     env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
-                    sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
+                    sh "mvn -DnewVersion='${NEW_VERSION}' versions:set"
                 }
             }
         }
         stage('Tag') {
             steps {
                 script {
-                    def mvnHome = tool 'Maven'
                     sh 'git config --global user.email "rahm.nathan@gmail.com"'
                     sh 'git config --global user.name "rahmnathan"'
                     sshagent(credentials: ['Github-Git']) {
                         sh 'mkdir -p /home/jenkins/.ssh'
                         sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
-                        sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+                        sh "mvn -Dtag=${NEW_VERSION} scm:tag"
                     }
                 }
             }
