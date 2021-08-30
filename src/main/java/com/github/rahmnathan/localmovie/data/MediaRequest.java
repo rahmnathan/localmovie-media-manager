a7c949424348529d8316b4ba02486b97d792a3ca
Nathan <rahm.nathan@protonmail.com>
2021-08-29 20:10:06 -0500

    Added MediaRequest validation.

--- src/main/java/com/github/rahmnathan/localmovie/data/MediaRequest.java
+++ src/main/java/com/github/rahmnathan/localmovie/data/MediaRequest.java
@@ -1,12 +1,15 @@
 package com.github.rahmnathan.localmovie.data;
 
+import lombok.NonNull;
 import lombok.Value;
 
 @Value
 public class MediaRequest {
+    @NonNull
     private final String path;
     private final Integer page;
     private final Integer resultsPerPage;
+    @NonNull
     private final MediaClient client;
     private final MediaOrder order;
 }
