b0b8f1881b26d6ea9f697999bf511f99429408d9
nathan <rahm.nathan@protonmail.com>
2023-09-20 18:55:41 -0700

    Adjust security.

--- src/main/java/com/github/rahmnathan/localmovie/web/SecurityConfig.java
+++ src/main/java/com/github/rahmnathan/localmovie/web/SecurityConfig.java
@@ -1,32 +1,18 @@
 package com.github.rahmnathan.localmovie.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 class SecurityConfig {
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
-    private final ApplicationContext context;
-
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
 
     @Order(1)
     @Bean
@@ -36,38 +22,11 @@ class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated();
-
-            try {
-                http.oauth2Login(Customizer.withDefaults())
-                        .logout(httpSecurityLogoutConfigurer -> {
-                            httpSecurityLogoutConfigurer.addLogoutHandler(keycloakLogoutHandler)
-                                    .logoutSuccessUrl("/");
-                        });
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         });
 
-        return http.build();
-    }
-
-    @Order(2)
-    @Bean
-    public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> {
-                    authorizeRequests.anyRequest()
-                            .authenticated();
-                })
-                .oauth2Login(Customizer.withDefaults());
-
-        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.oauth2Login(Customizer.withDefaults());
+        http.csrf().disable();
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
     }
 }
