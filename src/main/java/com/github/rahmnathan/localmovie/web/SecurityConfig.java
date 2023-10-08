c60b9f0bd1f6e0cb894e34b8bb94979f09e9e484
nathan <rahm.nathan@protonmail.com>
2023-10-08 15:03:58 -0700

    adjust jwt_bearer configuration.

--- src/main/java/com/github/rahmnathan/localmovie/web/SecurityConfig.java
+++ src/main/java/com/github/rahmnathan/localmovie/web/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,16 +18,14 @@ class SecurityConfig {
     @Order(1)
     @Bean
     public SecurityFilterChain anonymousAccessFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> {
-            authorizeRequests.requestMatchers("/actuator/**", "/forbidden.css")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated();
-        });
-
-        http.oauth2ResourceServer(Customizer.withDefaults());
-        http.oauth2Login(Customizer.withDefaults());
-        http.csrf().disable();
+        http.authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/actuator/**", "/forbidden.css")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
