d776d42a4908799b4cd651cc24a09c3c9742dad0
nrahm <nrahm@rei.com>
2023-10-31 10:58:58 -0700

    Add logging.

--- src/main/java/com/github/rahmnathan/localmovie/web/filter/AccessTokenFilter.java
+++ src/main/java/com/github/rahmnathan/localmovie/web/filter/AccessTokenFilter.java
@@ -2,12 +2,14 @@ package com.github.rahmnathan.localmovie.web.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
 import java.util.Optional;
 
+@Slf4j
 public class AccessTokenFilter implements Filter {
     public static final String ACCESS_TOKEN = "access_token";
 
@@ -19,11 +21,15 @@ public class AccessTokenFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         try {
+            log.info("AccessTokenFilter triggered.");
             final HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
             MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest(httpServletRequest);
 
             Optional<String> accessToken = Optional.ofNullable(httpServletRequest.getParameter(ACCESS_TOKEN));
-            accessToken.ifPresent(token -> mutableRequest.putHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token));
+            accessToken.ifPresent(token -> {
+                log.info("Adding access token to request.");
+                mutableRequest.putHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+            });
 
             filterChain.doFilter(mutableRequest, servletResponse);
         } finally {
