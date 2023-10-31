ea0d6e885af0ad421e9b2d5fc2ea8a275eacf794
nrahm <nrahm@rei.com>
2023-10-31 11:20:07 -0700

    Adjust token filter.

--- src/main/java/com/github/rahmnathan/localmovie/web/SecurityConfig.java
+++ src/main/java/com/github/rahmnathan/localmovie/web/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.github.rahmnathan.localmovie.web;
 
-import com.github.rahmnathan.localmovie.web.filter.AccessTokenFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,13 +19,19 @@ class SecurityConfig {
     @Order(1)
     @Bean
     public SecurityFilterChain anonymousAccessFilterChain(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new AccessTokenFilter(), BearerTokenAuthenticationFilter.class)
-                .authorizeHttpRequests(authorizeRequests ->
+        DefaultBearerTokenResolver resolver = new DefaultBearerTokenResolver();
+        resolver.setAllowUriQueryParameter(true);
+        resolver.setAllowFormEncodedBodyParameter(true);
+
+        http.authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/actuator/**", "/forbidden.css")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> {
+                    oauth2.jwt(Customizer.withDefaults());
+                    oauth2.bearerTokenResolver(resolver);
+                })
                 .oauth2Login(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
