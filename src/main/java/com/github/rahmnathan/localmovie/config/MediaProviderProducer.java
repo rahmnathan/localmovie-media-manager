a0fd5403c2bea1f610837ebb0aa74a947db0ac40
nathan <rahm.nathan@protonmail.com>
2024-09-09 20:38:06 -0700

    Use import.

--- src/main/java/com/github/rahmnathan/localmovie/config/MediaProviderProducer.java
+++ src/main/java/com/github/rahmnathan/localmovie/config/MediaProviderProducer.java
@@ -1,5 +1,6 @@
 package com.github.rahmnathan.localmovie.config;
 
+import com.github.rahmnathan.omdb.boundary.MediaProvider;
 import com.github.rahmnathan.omdb.boundary.MediaProviderOmdb;
 import com.github.rahmnathan.omdb.boundary.MediaProviderStub;
 import org.apache.camel.CamelContext;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class MediaProviderProducer {
 
     @Bean
-    public com.github.rahmnathan.omdb.boundary.MediaProvider createMovieProvider(CamelContext context, ProducerTemplate template, ServiceConfig serviceConfig){
+    public MediaProvider createMovieProvider(CamelContext context, ProducerTemplate template, ServiceConfig serviceConfig){
         if(serviceConfig.getOmdb().isEnabled()) {
             context.setUseMDCLogging(true);
             return new MediaProviderOmdb(context, template, serviceConfig.getOmdb().getApiKey());
