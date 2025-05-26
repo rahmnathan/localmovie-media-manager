7f9b39a4eda956a35f13b13b105146bae3be8ac7
nathan <rahm.nathan@protonmail.com>
2025-05-26 13:24:03 -0700

    Set it in code.

--- src/main/java/com/github/rahmnathan/Application.java
+++ src/main/java/com/github/rahmnathan/Application.java
@@ -5,11 +5,10 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jsonb.JsonbAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
 import java.nio.charset.Charset;
 
 @Slf4j
@@ -23,8 +22,10 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
+    @PostConstruct
     public void printEnv() {
+        System.setProperty("sun.jnu.encoding", "UTF-8");
+
         log.info("file.encoding: {}", Charset.defaultCharset().displayName());
         log.info("sun.jnu.encoding: {}", System.getProperty("sun.jnu.encoding"));
     }
