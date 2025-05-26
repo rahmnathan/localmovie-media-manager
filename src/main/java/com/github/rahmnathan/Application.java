a4320bc3dcddd945ae1ce46eaef38cd993609d3e
nathan <rahm.nathan@protonmail.com>
2025-05-26 11:54:39 -0700

    Log encoding.

--- src/main/java/com/github/rahmnathan/Application.java
+++ src/main/java/com/github/rahmnathan/Application.java
@@ -1,12 +1,18 @@
 package com.github.rahmnathan;
 
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jsonb.JsonbAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.nio.charset.Charset;
+
+@Slf4j
 @EnableAspectJAutoProxy
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "10m")
@@ -15,6 +21,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void printEnv() {
+        log.info("file.encoding: {}", Charset.defaultCharset().displayName());
+        log.info("sun.jnu.encoding: {}", System.getProperty("sun.jnu.encoding"));
     }
 }
 
