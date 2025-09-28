f0b33cc9fee8943d272ed7b143e8f15a3cbe2181
nathan <rahm.nathan@protonmail.com>
2025-09-28 14:05:10 -0700

    Remove reference.

--- src/main/js/VideoPlayer.jsx
+++ src/main/js/VideoPlayer.jsx
@@ -142,7 +142,7 @@ export function VideoPlayer() {
                 );
             }
         }
-    }, [url, mediaFile, castInitialized]);
+    }, [url, mediaFile]);
 
     useEffect(() => {
         if (mediaFile === null || mediaFile === undefined) return;
