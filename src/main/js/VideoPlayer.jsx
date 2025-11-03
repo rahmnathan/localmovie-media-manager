24ce9319fa5c5325bfb3c97f732de9af2f0ebac6
nathan <rahm.nathan@protonmail.com>
2025-11-03 15:43:05 -0800

    Use signed urls.

--- src/main/js/VideoPlayer.jsx
+++ src/main/js/VideoPlayer.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import Cookies from 'universal-cookie';
-import {trackPromise} from "react-promise-tracker";
-import {useParams} from 'react-router-dom';
-import {Description, Dialog, DialogPanel, DialogTitle} from "@headlessui/react";
+import { trackPromise } from 'react-promise-tracker';
+import { useParams } from 'react-router-dom';
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
 
 const cookies = new Cookies();
 
@@ -49,18 +49,17 @@ const dialogStyle = {
 };
 
 export function VideoPlayer() {
-
-    const [mediaFile, setMediaFile] = React.useState(null);
-    const [prompted, setPrompted] = React.useState(false);
-    const [resumePlayback, setResumePlayback] = React.useState(false);
-    const [canResumePlayback, setCanResumePlayback] = React.useState(false);
-    const [url, setUrl] = React.useState('');
-    const [signedUrls, setSignedUrls] = React.useState(null);
-    let { mediaId } = useParams();
+    const [mediaFile, setMediaFile] = useState(null);
+    const [prompted, setPrompted] = useState(false);
+    const [resumePlayback, setResumePlayback] = useState(false);
+    const [canResumePlayback, setCanResumePlayback] = useState(false);
+    const [url, setUrl] = useState('');
+    const [signedUrls, setSignedUrls] = useState(null);
+    const { mediaId } = useParams();
 
     useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1";
+        const script = document.createElement('script');
+        script.src = 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1';
         script.async = true;
         document.body.appendChild(script);
 
@@ -83,50 +82,48 @@ export function VideoPlayer() {
 
         context.addEventListener(cast.framework.CastContextEventType.SESSION_STATE_CHANGED, (event) => {
             if (event.sessionState === cast.framework.SessionState.SESSION_STARTED) {
-                console.log("Cast session started");
+                console.log('Cast session started');
             } else if (event.sessionState === cast.framework.SessionState.SESSION_ENDED) {
-                console.log("Cast session ended");
+                console.log('Cast session ended');
             }
         });
     };
 
+    // Fetch media details
     useEffect(() => {
         trackPromise(
-            fetch('/localmovie/v1/media/' + mediaId, {
+            fetch(`/localmovie/v1/media/${mediaId}`, {
                 method: 'GET',
                 headers: {
-                    'Accept': 'application/json'
+                    Accept: 'application/json'
                 }
-            }).then(response => response.json())
-                .then(media => setMediaFile(media))
-        )
-    }, []);
+            }).then(response => response.json()).then(media => setMediaFile(media))
+        );
+    }, [mediaId]);
 
+    // Fetch signed URLs
     useEffect(() => {
-
         trackPromise(
-            fetch('/localmovie/v1/media/' + mediaId + '/url/signed', {
+            fetch(`/localmovie/v1/media/${mediaId}/url/signed`, {
                 method: 'GET',
                 headers: {
-                    'Accept': 'application/json'
+                    Accept: 'application/json',
                 }
             }).then(response => response.json()).then(urls => setSignedUrls(urls)))
     }, [mediaId]);
 
     useEffect(() => {
-        if (mediaFile === null || mediaFile === undefined) return;
+        if (mediaFile && signedUrls) {
+            let startPosition = 0;
 
-        let startPosition = 0;
+            if (canResumePlayback && resumePlayback) {
+                const mediaView = mediaFile.mediaViews?.[0];
+                startPosition = mediaView?.position || 0;
+            }
 
-        if (canResumePlayback && resumePlayback) {
-            let mediaView = mediaFile.mediaViews[0];
-            let position = mediaView.position;
-            startPosition = position;
-
-            console.log("position: " + position);
+            const newUrl = `${window.location.origin}${signedUrls.stream}#t=${startPosition}`;
+            setUrl(newUrl);
         }
-
-        setUrl(window.location.origin + signedUrls.stream + "#t=" + startPosition)
     }, [resumePlayback, mediaFile, signedUrls]);
 
     useEffect(() => {
@@ -141,7 +138,7 @@ export function VideoPlayer() {
                     playOnCast(
                         url,
                         mediaFile.media.title,
-                        window.location.origin + signedUrls.poster
+                        `${window.location.origin}${signedUrls.poster}`
                     );
                 }
             }
@@ -161,87 +158,85 @@ export function VideoPlayer() {
     }, [url, signedUrls, mediaFile]);
 
     useEffect(() => {
-        if (mediaFile === null || mediaFile === undefined) return;
+        if (!mediaFile) return;
 
-        let mediaViews = mediaFile.mediaViews;
-        if(mediaViews !== null && mediaViews !== undefined && mediaViews.length !== 0) {
-            let mediaView = mediaViews[0];
-            let position = mediaView.position;
-            setCanResumePlayback(parseFloat(position) > 0)
+        const mediaViews = mediaFile.mediaViews;
+        if (mediaViews?.length > 0) {
+            const position = mediaViews[0]?.position || 0;
+            setCanResumePlayback(position > 0);
         }
     }, [mediaFile]);
 
-    function saveProgress(content) {
-        cookies.set('progress-' + mediaId, content.playedSeconds, {sameSite: 'strict'});
+    const saveProgress = (content) => {
+        cookies.set(`progress-${mediaId}`, content.playedSeconds, { sameSite: 'strict' });
 
-        if(content.playedSeconds > 0) {
-            fetch(signedUrls.updatePosition.split('?')[0] + '/' + content.playedSeconds + '?' + signedUrls.updatePosition.split('?')[1], {
-                method: 'PATCH',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
+        if (content.playedSeconds > 0) {
+            fetch(
+                `${signedUrls.updatePosition.split('?')[0]}/${content.playedSeconds}?${signedUrls.updatePosition.split('?')[1]}`,
+                {
+                    method: 'PATCH',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
                 }
-            })
+            );
         }
-    }
+    };
 
     const playOnCast = (mediaUrl, title, imageUrl) => {
         if (!window.cast) {
-            console.warn("Cast framework not ready");
+            console.warn('Cast framework not ready');
             return;
         }
 
         const session = window.cast.framework.CastContext.getInstance().getCurrentSession();
         if (!session) {
-            console.log("No cast session active.");
+            console.log('No cast session active.');
             return;
         }
 
-        const mediaInfo = new window.chrome.cast.media.MediaInfo(mediaUrl.split('#')[0], "video/mp4");
+        const mediaInfo = new window.chrome.cast.media.MediaInfo(mediaUrl.split('#')[0], 'video/mp4');
         mediaInfo.metadata = new window.chrome.cast.media.MovieMediaMetadata();
         mediaInfo.metadata.title = title;
         mediaInfo.metadata.images = [{ url: imageUrl }];
 
         const request = new window.chrome.cast.media.LoadRequest(mediaInfo);
         session.loadMedia(request).then(
-            () => console.log("Cast media loaded successfully"),
-            (errorCode) => console.error("Error loading cast media", errorCode)
+            () => console.log('Cast media loaded successfully'),
+            (errorCode) => console.error('Error loading cast media', errorCode)
         );
     };
 
-    if(mediaFile !== null && mediaFile !== undefined &&
-        signedUrls !== null && signedUrls !== undefined &&
-        url !== null && url !== undefined) {
+    if (mediaFile && signedUrls && url) {
         const hasCastSession = window.cast?.framework
             ? window.cast.framework.CastContext.getInstance().getCurrentSession()
             : null;
 
         return (
             <div style={videoPlayerStyle}>
-                <Dialog open={!prompted && canResumePlayback}
-                        onClose={() => setPrompted(true)}
-                        style={dialogStyle}>
-                    <div>
-                        <DialogPanel>
-                            <DialogTitle>{mediaFile.media.title}</DialogTitle>
-                            <img src={window.location.origin + signedUrls.poster} alt=''/>
-                            <Description>Pick up from where you left off, or start from the beginning!</Description>
-                            <div>
-                                <button onClick={() => {
-                                    setResumePlayback(false)
-                                    setPrompted(true)
-                                }}
-                                        style={{marginRight: 5}}>Play
-                                </button>
-                                <button onClick={() => {
-                                    setResumePlayback(true)
-                                    setPrompted(true)
-                                }}>Resume
-                                </button>
-                            </div>
-                        </DialogPanel>
-                    </div>
+                <Dialog open={!prompted && canResumePlayback} onClose={() => setPrompted(true)} style={dialogStyle}>
+                    <DialogPanel>
+                        <DialogTitle>{mediaFile.media.title}</DialogTitle>
+                        <img src={`${window.location.origin}${signedUrls.poster}`} alt="" />
+                        <Description>Pick up from where you left off, or start from the beginning!</Description>
+                        <div>
+                            <button onClick={() => {
+                                setResumePlayback(false);
+                                setPrompted(true);
+                            }} style={{ marginRight: 5 }}>
+                                Play
+                            </button>
+                            <button onClick={() => {
+                                setResumePlayback(true);
+                                setPrompted(true);
+                            }}>
+                                Resume
+                            </button>
+                        </div>
+                    </DialogPanel>
                 </Dialog>
+
                 {!hasCastSession && (
                     <ReactPlayer
                         url={url}
@@ -253,6 +248,8 @@ export function VideoPlayer() {
                 )}
                 <div style={backgroundTintStyle}/>
             </div>
-        )
+        );
     }
+
+    return null;
 }
