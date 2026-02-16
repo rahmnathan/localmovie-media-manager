34a857b0a522bf141b5d51a86686485b21d36681
nathan <rahm.nathan@protonmail.com>
2026-02-16 15:19:31 -0800

    Ignore unknown fields.

--- src/main/java/com/github/rahmnathan/localmovie/media/recommendation/ollama/OllamaResponse.java
+++ src/main/java/com/github/rahmnathan/localmovie/media/recommendation/ollama/OllamaResponse.java
@@ -1,10 +1,12 @@
 package com.github.rahmnathan.localmovie.media.recommendation.ollama;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OllamaResponse {
     private String model;
     private String response;
--- src/main/java/com/github/rahmnathan/localmovie/media/recommendation/ollama/OllamaTagsResponse.java
+++ src/main/java/com/github/rahmnathan/localmovie/media/recommendation/ollama/OllamaTagsResponse.java
@@ -1,5 +1,6 @@
 package com.github.rahmnathan.localmovie.media.recommendation.ollama;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,11 +8,13 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OllamaTagsResponse {
     private List<OllamaModel> models;
 
     @Data
     @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class OllamaModel {
         private String name;
         private String model;
